fec954aae0b131b59bab50ff7e4976359d923838
Dany Paredes <danywalls4@gmail.com>
2023-09-02 08:35:25 +0200

    feat: :art: add styles for image upload

--- src/app/components/image-upload/image-upload.component.ts
+++ src/app/components/image-upload/image-upload.component.ts
@@ -8,10 +8,11 @@ import { FakeImageUploadService } from 'src/app/services/fake-image-upload.servi
   styleUrls: ['./image-upload.component.css'],
 })
 export class ImageUploadComponent {
-
-  constructor(private fakeImageUploadService: FakeImageUploadService) { }
+  constructor(private fakeImageUploadService: FakeImageUploadService) {}
 
   selectedImage!: FileList;
+  imagesUrl: Array<string> = [];
+  uploading = false;
 
   onImageSelected(event: Event): void {
     const inputElement = event.target as HTMLInputElement;
@@ -22,11 +23,21 @@ export class ImageUploadComponent {
   }
 
   uploadImage(): void {
-    this.fakeImageUploadService.uploadImage(this.selectedImage[0]);
+    if (this.selectedImage) {
+      this.uploadFiles(this.selectedImage);
+    }
   }
 
-  imagesUrl: Array<string> = [];
-  uploading = false;
+  private uploadFiles(images: FileList) {
+    this.uploading = true;
+    for (let index = 0; index < images.length; index++) {
+      const element = images[index];
+      this.fakeImageUploadService.uploadImage(element).subscribe((p) => {
+        this.imagesUrl.push(p);
+        this.uploading = false;
+      });
+    }
+  }
 
   onDrop(event: DragEvent): void {
     event.preventDefault();
@@ -34,25 +45,6 @@ export class ImageUploadComponent {
       this.uploadFiles(event.dataTransfer.files);
     }
   }
-
-  private uploadFiles(images: FileList) {
-    for (let index = 0; index < images.length; index++) {
-      const element = images[index];
-
-      this.fakeImageUploadService
-        .uploadImage(element)
-        .pipe(
-          catchError(() => {
-            tap(() => (this.uploading = false));
-            return EMPTY;
-          })
-        )
-        .subscribe((p) => {
-          this.imagesUrl.push(p);
-        });
-    }
-  }
-
   onDragOver(event: DragEvent): void {
     event.preventDefault();
   }
